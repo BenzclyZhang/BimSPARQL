2594f257e57731687f31b956d239e248b02317cc
BenzclyZhang <zhangchi.ben@gmail.com>
2017-04-14 20:11:25 +0200

    maven changed

---
+++ src/main/java/nl/tue/ddss/bimsparql/ModelLoader.java
@@ -0,0 +1,12 @@
+package nl.tue.ddss.bimsparql;
+
+public class ModelLoader {
+	
+	public static void loadModel(String file){
+	}
+	
+	public static void loadModel(String file, String tdbPath){
+		
+	}
+
+}
--- src/main/java/nl/tue/ddss/bimsparql/function/geom/aabb.java
+++ src/main/java/nl/tue/ddss/bimsparql/function/geom/aabb.java
@@ -1,14 +1,43 @@
 package nl.tue.ddss.bimsparql.function.geom;
 
+import java.io.IOException;
+
 import com.hp.hpl.jena.sparql.expr.NodeValue;
 import com.hp.hpl.jena.sparql.function.FunctionBase1;
+
+import nl.tue.ddss.bimsparql.geometry.Geometry;
+import nl.tue.ddss.bimsparql.geometry.ewkt.EwktReader;
+import nl.tue.ddss.bimsparql.geometry.ewkt.EwktWriter;
+import nl.tue.ddss.bimsparql.geometry.ewkt.WktParseException;
+import nl.tue.ddss.bimsparql.geometry.ewkt.WktWriteException;
+import nl.tue.ddss.bimsparql.geometry.visitor.AABBVisitor;
 
 public class aabb extends FunctionBase1{
 
 	@Override
 	public NodeValue exec(NodeValue v) {
-		// TODO Auto-generated method stub
-		return null;
+		EwktReader er=new EwktReader(v.getString());
+		try {
+			Geometry g=er.readGeometry();
+			AABBVisitor visitor=new AABBVisitor();
+			g.accept(visitor);
+			EwktWriter ew=new EwktWriter("");
+			ew.writeRec(visitor.getAABB().toPolyhedralSurface());
+			
+			return NodeValue.makeString(ew.getString());
+		} catch (WktParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return NodeValue.nvNothing;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return NodeValue.nvNothing;
+		} catch (WktWriteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return NodeValue.nvNothing;
+		}
 	}
 
 }
