fb1d2023a7e10a70635f5c17f003b3abe02c2d5e
BenzclyZhang <zhangchi.ben@gmail.com>
2017-04-14 20:17:36 +0200

    license added

--- src/main/java/nl/tue/ddss/convert/rdf2ifc/IfcObject.java
+++ src/main/java/nl/tue/ddss/convert/rdf2ifc/IfcObject.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright (C) 2017 Chi Zhang
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 /*
  Copyright (c) 2014 Jyrki Oraskari, Aalto University (jyrki [dot] oraskari [at] aalto [dot] fi)
  Copyright (c) 2014 Pieter Pauwels, Ghent University (modifications - pipauwel [dot] pauwels [at] ugent [dot] be / pipauwel [at] gmail [dot] com)
